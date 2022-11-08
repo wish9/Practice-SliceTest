ad48f950b8a6b68d39309b81225b00a7adec64d4
hjs6877 <hjs6877@gmail.com>
2022-11-08 21:35:09 +0900

    .

--- src/test/java/com/codestates/slice/controller/basic/ControllerTestDefaultStructure.java
+++ src/test/java/com/codestates/slice/controller/basic/ControllerTestDefaultStructure.java
@@ -16,10 +16,10 @@ public class ControllerTestDefaultStructure {
     // (4)
     @Test
     public void postMemberTest() {
-        // (5) 테스트용 request body 생성
+        // given (5) 테스트용 request body 생성
 
-        // (6) MockMvc 객체로 테스트 대상 Controller 호출
+        // when (6) MockMvc 객체로 테스트 대상 Controller 호출
 
-        // (7) Controller 핸들러 메서드에서 응답으로 수신한 HTTP Status 및 response body 검증
+        // then (7) Controller 핸들러 메서드에서 응답으로 수신한 HTTP Status 및 response body 검증
     }
 }
